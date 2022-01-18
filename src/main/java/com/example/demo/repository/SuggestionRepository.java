1e221359287120e6fd2ca562305080289c0c5b19
Mohsen <malakouti.aero@gmail.com>
2022-01-19 01:00:28 +0330

    Suggestion repository modified for implementing a specification interface for two method in the suggestion class.

--- src/main/java/com/example/demo/repository/SuggestionRepository.java
+++ src/main/java/com/example/demo/repository/SuggestionRepository.java
@@ -3,10 +3,11 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Suggestion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface SuggestionRepository extends JpaRepository<Suggestion, Integer> {
+public interface SuggestionRepository extends JpaRepository<Suggestion, Integer> , JpaSpecificationExecutor<Suggestion> {
 
     List<Suggestion> findSuggestionByOrderIdOrderBySuggestedPriceAsc(Integer orderId);
     List<Suggestion> findSuggestionByOrderId(Integer orderId);
