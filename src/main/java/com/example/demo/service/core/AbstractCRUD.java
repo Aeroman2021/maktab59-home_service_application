0516fb5d8c13b788a6a3fec398bbc8df4813aeb0
Mohsen <malakouti.aero@gmail.com>
2022-01-19 00:55:48 +0330

    The abstractCrud Included all neccessary CRUD methods.

---
+++ src/main/java/com/example/demo/service/core/AbstractCRUD.java
@@ -0,0 +1,44 @@
+package com.example.demo.service.core;
+
+import com.example.demo.Exceptions.EntityNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class AbstractCRUD <T,ID> {
+
+    private JpaRepository<T,ID> jpaRepository;
+
+    public void setJpaRepository(JpaRepository<T, ID> jpaRepository) {
+        this.jpaRepository = jpaRepository;
+    }
+
+    @Transactional
+    public T save(T entity){
+        return jpaRepository.save(entity);
+    }
+
+    @Transactional
+    public T update(T entity){
+        return jpaRepository.save(entity);
+    }
+
+
+    public T loadById(ID id){
+        return  jpaRepository.findById(id).orElseThrow(
+                ()-> new EntityNotFoundException("Entity with id " + id + " not existed!")
+        );
+    }
+
+    public List<T> loadAll(){
+        return jpaRepository.findAll();
+    }
+
+    @Transactional
+    public void deleteById(ID id){
+        jpaRepository.deleteById(id);
+    }
+
+
+}
