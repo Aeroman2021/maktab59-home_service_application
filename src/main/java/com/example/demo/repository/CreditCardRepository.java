a0fe841aa2266419c56782f8603e6c62003edc2a
Mohsen <malakouti.aero@gmail.com>
2022-01-19 01:04:28 +0330

    Creditcard and admin repository added.

---
+++ src/main/java/com/example/demo/repository/CreditCardRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.CreditCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreditCardRepository extends JpaRepository<CreditCard,Integer> {
+
+    CreditCard findCreditCardByCustomerId(Integer customerId);
+}
