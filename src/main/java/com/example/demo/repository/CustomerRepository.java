aaaba5dabaf5d0d28ab5e76a3b7ea2de999ee722
Mohsen <malakouti.aero@gmail.com>
2022-01-21 11:56:14 +0330

    A method added to list customers based on the registration date.

--- src/main/java/com/example/demo/repository/CustomerRepository.java
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
 import java.util.List;
 
 @Repository
@@ -13,4 +15,5 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     List<Customer> findCustomerByFirstName(String firstName);
     List<Customer> findCustomerByLastName(String lastName);
     Customer findCustomerByEmail(String Email);
+    List<Customer> findCustomersByRegisterDateAfter(Date registerDate);
 }
