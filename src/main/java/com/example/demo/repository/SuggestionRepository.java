ec441c0f128bc616d1ec4267ecafc97aef59df9d
Mohsen <malakouti.aero@gmail.com>
2022-01-21 11:52:04 +0330

    A method added to list the suggestions submitted by the techs.

--- src/main/java/com/example/demo/repository/SuggestionRepository.java
+++ src/main/java/com/example/demo/repository/SuggestionRepository.java
@@ -2,14 +2,21 @@ package com.example.demo.repository;
 
 
 import com.example.demo.model.Suggestion;
+import com.example.demo.model.enums.SuggestionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface SuggestionRepository extends JpaRepository<Suggestion, Integer> , JpaSpecificationExecutor<Suggestion> {
+public interface SuggestionRepository extends JpaRepository<Suggestion, Integer>, JpaSpecificationExecutor<Suggestion> {
 
     List<Suggestion> findSuggestionByOrderIdOrderBySuggestedPriceAsc(Integer orderId);
+
     List<Suggestion> findSuggestionByOrderId(Integer orderId);
+
+    List<Suggestion> findSuggestionsByTechnicianId(Integer technicianId);
+
+
 
 }
