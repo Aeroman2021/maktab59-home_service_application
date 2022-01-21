fce7cf3959a4da50ca84cacaf989b67e1b29dc82
Mohsen <malakouti.aero@gmail.com>
2022-01-21 11:58:07 +0330

    the creditcarddto added to the dto.

--- src/main/java/com/example/demo/dto/order/AddOrderForCustomerInputArgsDto.java
+++ src/main/java/com/example/demo/dto/order/AddOrderForCustomerInputArgsDto.java
@@ -4,6 +4,7 @@ package com.example.demo.dto.order;
 import com.example.demo.dto.address.SubmittedAddressDto;
 import com.example.demo.dto.creditcard.CreditCardInputDto;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,11 +12,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AddOrderForCustomerInputArgsDto {
     private Integer customerId;
     private Integer subServiceId;
     private Integer SuggestionId;
-    CreditCardInputDto creditCardInputDto;
+    private CreditCardInputDto creditCardInputDto;
     private SubmittedAddressDto submittedAddressDto;
     private InputOrderInformationDto submittedOrderDto;
 
