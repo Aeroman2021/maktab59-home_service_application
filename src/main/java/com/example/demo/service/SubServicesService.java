526f6dbdae01f96f36e55350f338a1458f615ea9
Mohsen <malakouti.aero@gmail.com>
2022-02-03 07:21:28 +0330

    the method for submitting technicians for a subservice modified.

--- src/main/java/com/example/demo/service/SubServicesService.java
+++ src/main/java/com/example/demo/service/SubServicesService.java
@@ -1,14 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.Exceptions.UserException;
 import com.example.demo.dto.subServices.RegisterSubServiceInputDto;
 import com.example.demo.dto.subServices.SubServiceOutputDto;
-import com.example.demo.dto.subServices.SubmitTechsToSubServicesInputDto;
 import com.example.demo.model.SubServices;
 import com.example.demo.model.Technician;
 import com.example.demo.repository.SubServiceRepository;
 import com.example.demo.service.core.AbstractCRUD;
 import com.example.demo.service.core.EntityConvertor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,7 @@ import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SubServicesService extends AbstractCRUD<SubServices, Integer> implements
@@ -46,14 +48,23 @@ public class SubServicesService extends AbstractCRUD<SubServices, Integer> imple
     }
 
     @Transactional
-    public SubServiceOutputDto submitTechnicianToSubService(Integer subserviceId,Integer technicianId){
+    public SubServiceOutputDto submitTechnicianToSubService(Integer subServiceId, Integer technicianId) {
+        if (isDuplicateTechnicianSubmissionForTheSubService(subServiceId, technicianId)) {
+            log.error("The technician with id {} has submitted for the subservice with id {} ", technicianId, subServiceId);
+            throw new UserException("The technician has submitted for the subservice");
+        }
         List<Technician> technicianList = new ArrayList<>();
         Technician technician = technicianService.loadById(technicianId);
         technicianList.add(technician);
-        SubServices subServices = super.loadById(subserviceId);
+        SubServices subServices = super.loadById(subServiceId);
         subServices.setTechnicians(technicianList);
         super.update(subServices);
         return convertEntityToOutputDto(subServices);
+    }
+
+    public boolean isDuplicateTechnicianSubmissionForTheSubService(Integer subServiceId, Integer technicianId) {
+        Technician technician = technicianService.loadById(technicianId);
+        return super.loadById(subServiceId).getTechnicians().contains(technician);
     }
 
     @Override
