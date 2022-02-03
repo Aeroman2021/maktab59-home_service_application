e6cd3e988ced41628a9b2ab05501968472e57448
Mohsen <malakouti.aero@gmail.com>
2022-02-03 07:16:43 +0330

    The filteration methods changed.

--- src/main/java/com/example/demo/service/TechnicianService.java
+++ src/main/java/com/example/demo/service/TechnicianService.java
@@ -11,6 +11,7 @@ import com.example.demo.repository.TechnicianRepository;
 import com.example.demo.service.core.AbstractCRUD;
 import com.example.demo.service.core.UsersConvertor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,43 +21,55 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class TechnicianService extends AbstractCRUD<Technician,Integer> implements UsersConvertor<Technician, RegisterTechnicianInputDto> {
+public class TechnicianService extends AbstractCRUD<Technician, Integer>
+        implements UsersConvertor<Technician, RegisterTechnicianInputDto> {
 
 
     private final TechnicianRepository technicianRepository;
     private final PasswordValidator passwordValidator;
 
     @PostConstruct
-    public void init(){
+    public void init() {
         setJpaRepository(technicianRepository);
     }
 
     @Transactional
     public UserOutputDto addTechnician(RegisterTechnicianInputDto technicianInputDto) {
-        if (emailAndPasswordIsValid(technicianInputDto)) {
-            Technician technician = convertInputToEntity(technicianInputDto);
-            super.save(technician);
-            return new UserOutputDto(technician.getId(), technician.getFirstName(), technician.getLastName());
-        } else
-            throw new UserException("Unable to save the Technician");
+        if (!emailAndPasswordIsValid(technicianInputDto)) {
+            log.error("Unable to save technician with the username {}", technicianInputDto.getUsername());
+            throw new UserException("Unable to save the technician");
+        }
+        log.debug("the  user with username {} saved to the database.", technicianInputDto.getUsername());
+        Technician savedTechnician = super.save(convertInputToEntity(technicianInputDto));
+        return new UserOutputDto(savedTechnician.getId(), savedTechnician.getFirstName(),
+                savedTechnician.getLastName());
     }
 
     @Transactional
-    public void updatePassword(UserLoginDto technicianLoginDto, String newPassword) {
-        if (findTechByUsernameAndPassword(technicianLoginDto) != null
-                && passwordValidator.passwordChecker(newPassword)) {
-            Technician technician = super.loadById(technicianLoginDto.getId());
-            technician.setPassword(newPassword);
-        } else
+    public void updatePassword(UserLoginDto customerLoginDto) {
+        if (findTechByUsernameAndPassword(customerLoginDto) == null
+                || !passwordValidator.passwordChecker(customerLoginDto.getNewPassword())) {
+            log.error("Unable to change password for the user with id {}", customerLoginDto.getId());
             throw new UserException("Unable to change the password!");
+        }
+        if (customerLoginDto.getNewPassword().equals(customerLoginDto.getOldPassword())) {
+            log.error("Duplicated input password for the user with id {} ", customerLoginDto.getId());
+            throw new UserException("Duplicated password");
+        }
+        Technician technician = super.loadById(customerLoginDto.getId());
+        technician.setPassword(customerLoginDto.getNewPassword());
+        super.update(technician);
     }
+
+
 
     public Technician findTechByUsernameAndPassword(UserLoginDto customerLoginDto) {
         return technicianRepository
                 .findTechnicianByUsernameAndPassword(customerLoginDto.getUsername()
-                        , customerLoginDto.getPassword());
+                        , customerLoginDto.getOldPassword());
     }
 
 
@@ -69,11 +82,11 @@ public class TechnicianService extends AbstractCRUD<Technician,Integer> implemen
         return loadTechsByByEmail(technicianInputDto.getEmail()) == null;
     }
 
-    public List<Technician> filterTechniciansByRegisterDate(Date date){
+    public List<Technician> filterTechniciansByRegisterDate(Date date) {
         return technicianRepository.findTechniciansByRegisterDateAfter(date);
     }
 
-    public List<Technician> filterTechniciansByRegStatus(RegisterStatus status){
+    public List<Technician> filterTechniciansByRegStatus(RegisterStatus status) {
         return technicianRepository.findTechniciansByStatus(status);
     }
 
@@ -103,12 +116,12 @@ public class TechnicianService extends AbstractCRUD<Technician,Integer> implemen
         return technicianRepository.findTechnicianByEmail(email);
     }
 
-    public UserOutputDto filterTechsByEmail(String email){
+    public UserOutputDto filterTechsByEmail(String email) {
         return convertEntityToOutputDto(technicianRepository.findTechnicianByEmail(email));
     }
 
 
-    public List<UserOutputDto> loadTechsWithPointsGreaterThanLimit(Double point){
+    public List<UserOutputDto> loadTechsWithPointsGreaterThanLimit(Double point) {
         List<UserOutputDto> submittedTechsList = new ArrayList<>();
         for (Technician technician : technicianRepository.findTechnicianByAveragePointGreaterThan(point)) {
             UserOutputDto userOutputDto = convertEntityToOutputDto(technician);
@@ -127,6 +140,14 @@ public class TechnicianService extends AbstractCRUD<Technician,Integer> implemen
                 .build();
     }
 
+    public UserOutputDto convertLoginToOutputDto(UserLoginDto loginDto) {
+        return UserOutputDto.builder()
+                .id(loginDto.getId())
+                .firstName(loginDto.getFirstName())
+                .lastname(loginDto.getLastname())
+                .build();
+    }
+
     @Override
     public Technician convertInputToEntity(RegisterTechnicianInputDto technicianInputDto) {
         return Technician.builder()
@@ -139,7 +160,8 @@ public class TechnicianService extends AbstractCRUD<Technician,Integer> implemen
                 .status(RegisterStatus.NEW)
                 .credit(0d)
                 .build();
-
     }
+
+
 
 }
